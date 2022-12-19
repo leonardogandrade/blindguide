5212f6eded452f10fd2228413f4c971c3ac875df
leonardogandrade <leonardo.g.andrade@gmail.com>
2022-12-19 14:47:41 -0300

    detector

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from src.openCV.Detector import *
 
 modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
 
+imagePath = "assets/federer.jpg"
+
 classFile = "src/config/coco.names"
 detector = Detector()
 detector.readClasses(classFile)
 detector.downloadModel(modelURL)
+detector.loadModel()
+detector.predictImage(imagePath)
--- src/openCV/Detector.py
+++ src/openCV/Detector.py
@@ -1,5 +1,5 @@
-# import cv2
-# import tensorflow as tf
+import cv2
+import tensorflow as tf
 import time 
 import os 
 import numpy as np
@@ -29,3 +29,48 @@ class Detector:
 
         get_file(fname=fileName,
         origin=modelURL, cache_dir=self.cacheDir, cache_subdir="checkpoints", extract=True)
+
+    def loadModel(self):
+        print("Loading Model" + self.modelName)
+        tf.keras.backend.clear_session()
+        self.model = tf.saved_model.load(os.path.join(self.cacheDir, "checkpoints", self.modelName, "saved_model"))
+
+        print("Model " + self.modelName + " was loaded successfully...")
+
+    def createBoudingBox(self, image):
+        inputTensor = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+        inputTensor = tf.convert_to_tensor(inputTensor, dtype=tf.uint8)
+        inputTensor = inputTensor[tf.newaxis, ...]
+
+        detections = self.model(inputTensor)
+        bboxs = detections['detection_boxes'][0].numpy()
+        classIndexes = detections['detection_classes'][0].numpy().astype(np.int32)
+        classScores = detections['detection_scores'][0].numpy()
+
+        imH, imW, imC = image.shape
+
+        if len(bboxs) != 0:
+            for i in range(0, len(bboxs)):
+                bbox = tuple(bboxs[i].tolist())
+                classConfidence = round(100 * classScores[i])
+                classIndex = classIndexes[i]
+
+                classLabelText = self.classesList[classIndex]
+                classColor = self.colorList[classIndex]
+
+                displayText = '{} : {}%'.format(classLabelText, classConfidence)
+
+                ymin, xmin, ymax, xmax = bbox
+
+                print(ymin, xmin, ymax, xmax)
+                break
+
+    def predictImage(self, imagePath):
+        image = cv2.imread(imagePath)
+
+        self.createBoudingBox(image)
+
+        cv2.imshow("Result", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
